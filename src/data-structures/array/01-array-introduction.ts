f6ede9d92d7b961fa6825381581367cf3d1b0002
Rahul Rana <me.rahulrana@outlook.com>
2025-02-18 01:03:21 +0000

    doc: adds Leetcode problems list

--- src/data-structures/array/01-array-introduction.ts
+++ src/data-structures/array/01-array-introduction.ts
@@ -1,8 +1,8 @@
 /**
  * Generic Array class implementation.
- * This class provides basic functionalities like adding, removing, 
+ * This class provides basic functionalities like adding, removing,
  * retrieving elements, and checking the size of the array.
- * 
+ *
  * @template T - The type of elements stored in the array.
  */
 export class Array<T> {
@@ -11,7 +11,7 @@ export class Array<T> {
 
     /**
      * Adds an item to the array.
-     * 
+     *
      * @param item - The item of type T to be added to the array.
      */
     add(item: T): void {
@@ -20,7 +20,7 @@ export class Array<T> {
 
     /**
      * Removes the last item from the array.
-     * 
+     *
      * @returns The removed item of type T, or undefined if the array is empty.
      */
     remove(): T | undefined {
@@ -29,7 +29,7 @@ export class Array<T> {
 
     /**
      * Retrieves an item from the array at a specific index.
-     * 
+     *
      * @param index - The position of the item in the array (0-based index).
      * @returns The item at the specified index, or undefined if the index is out of bounds.
      */
@@ -39,7 +39,7 @@ export class Array<T> {
 
     /**
      * Returns the number of items in the array.
-     * 
+     *
      * @returns The length of the array.
      */
     size(): number {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Main
-export { Array } from './data-structures/array/array';
+export { Array } from './data-structures/array/01-array-introduction';
 export { LinkedList } from './data-structures/linked-list/linkedList';
 export { bubbleSort } from './algorithms/sorting/bubbleSort';
 export { binarySearch } from './algorithms/searching/binarySearch';
